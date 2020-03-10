024c1ad8b8b840110ded2206e2a0b1645dc0b95a
David Rogers <drogers@ucvts.tec.nj.us>
2020-03-10 08:34:49 -0400

    3/10 commit

--- src/GSONread.java
+++ src/GSONread.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import com.google.gson.Gson;
@@ -11,20 +12,38 @@ public class GSONread {
 	static Word[] wordObj;
 
 	public static void main(String[] args) {
-		returnWords();
+		returnWords(true);
 	}
 
-	public static List<Word> returnWords() {
+	public static ArrayList<Word> returnWords(boolean aToZ) {
 		Gson gson = new Gson();
 
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("words.json"));
 
-			wordObj = gson.fromJson(br, Word[].class);
+			List<Word> list = Dictionary.getWords();
 
-			List<Word> list = Arrays.asList(wordObj);
-			list.sort(Comparator.comparing(Word::toString));
-			return list;
+			Collections.sort(list, new Comparator<Word>() {
+				@Override
+				public int compare(Word word2, Word word1) {
+
+					return word1.word.compareTo(word2.word);
+				}
+			});
+
+			ArrayList<Word> sortedWords = new ArrayList(list);
+
+			if (aToZ) {
+				ArrayList<Word> temp = new ArrayList<Word>();
+				for (int i = sortedWords.size() - 1; i > -1; i--) {
+					temp.add(sortedWords.get(i));
+				}
+				sortedWords = temp;
+			}
+
+			Dictionary.editJson(sortedWords);
+
+			return sortedWords;
 
 		} catch (IOException e) {
 			e.printStackTrace();
