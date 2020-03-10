b391cb439274ff0fd66d255fee6c7198da99740d
David Rogers <drogers@ucvts.tec.nj.us>
2020-03-10 08:38:19 -0400

    import statements added

--- src/RemoveWord.java
+++ src/RemoveWord.java
@@ -1,3 +1,12 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RemoveWord {
 
