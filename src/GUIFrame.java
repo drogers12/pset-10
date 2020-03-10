12b8d3391b488bb49e0fe00f32ca8ed5e9401780
David Rogers <drogers@ucvts.tec.nj.us>
2020-03-10 08:36:53 -0400

    import statements added

--- src/GUIFrame.java
+++ src/GUIFrame.java
@@ -1,13 +1,4 @@
-/** things to do:
- * 
- * 1) create linkedhashmap
- * 2) put json words into linkedhashmap
- * 3) create functions to work with GUI and add to linkedhashmap
- *
- */
-
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -15,18 +6,20 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import javax.swing.JComboBox;
-import javax.swing.JTextArea;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
-import java.awt.Component;
-import javax.swing.Box;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JScrollPane;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JTextPane;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import com.google.gson.*;
 
 public class GUIFrame {
 
@@ -34,6 +27,7 @@ public class GUIFrame {
 	private JTextField searchBox;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField wordInfoDisplay;
+	private List<Word> jsonArray;
 
 	/**
 	 * Launch the application.
@@ -66,14 +60,15 @@ public class GUIFrame {
 		frmDictionary.setTitle("Dictionary");
 		frmDictionary.setBounds(100, 100, 686, 535);
 		frmDictionary.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JButton addWordBtn = new JButton("Add Word");
+		addWordBtn.setFocusPainted(false);
 		addWordBtn.setBounds(6, 6, 117, 29);
 		addWordBtn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// new field/frame? appears to add word to dictionary
-				
+
 			}
 		});
 		addWordBtn.setFont(new Font("Chalkboard", Font.PLAIN, 13));
@@ -83,75 +78,78 @@ public class GUIFrame {
 		});
 		frmDictionary.getContentPane().setLayout(null);
 		frmDictionary.getContentPane().add(addWordBtn);
-		
+
 		JButton removeWordBtn = new JButton("Remove Word");
 		removeWordBtn.setBounds(122, 6, 117, 29);
 		removeWordBtn.setFont(new Font("Chalkboard", Font.PLAIN, 13));
+		removeWordBtn.setFocusPainted(false);
 		frmDictionary.getContentPane().add(removeWordBtn);
-		
+
 		searchBox = new JTextField("Search...");
 		searchBox.setBounds(6, 34, 233, 38);
 		searchBox.setFont(new Font("Chalkboard", Font.PLAIN, 18));
 		searchBox.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent e) {
-				if(searchBox.getText().equals("Search...")) {
+				if (searchBox.getText().equals("Search...")) {
 					searchBox.setText("");
 				}
 			}
+
 			@Override
 			public void focusLost(FocusEvent e) {
-				if(!searchBox.getText().equals("Search...")) {
+				if (!searchBox.getText().equals("Search...")) {
 					searchBox.setText("Search...");
 				}
 			}
 		});
 		frmDictionary.getContentPane().add(searchBox);
 		searchBox.setColumns(10);
-		
+
 		JRadioButton aToZRdoBtn = new JRadioButton("A to Z");
-		aToZRdoBtn.setBounds(6, 75, 70, 23);
+		aToZRdoBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// sort A to Z
+				jsonArray = GSONread.returnWords();
+				
+			}
+		});
+		aToZRdoBtn.setBounds(6, 70, 141, 23);
 		aToZRdoBtn.setFont(new Font("Chalkboard", Font.PLAIN, 13));
 		aToZRdoBtn.setSelected(true);
+		aToZRdoBtn.setFocusPainted(false);
 		buttonGroup.add(aToZRdoBtn);
 		frmDictionary.getContentPane().add(aToZRdoBtn);
-		
+
 		JRadioButton zToARdoBtn = new JRadioButton("Z to A");
-		zToARdoBtn.setBounds(6, 100, 70, 23);
+		zToARdoBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// sort Z to A
+				jsonArray = GSONread.returnWords();
+				System.out.println(jsonArray);
+			}
+		});
+		zToARdoBtn.setBounds(6, 95, 141, 23);
 		zToARdoBtn.setFont(new Font("Chalkboard", Font.PLAIN, 13));
+		zToARdoBtn.setFocusPainted(false);
 		buttonGroup.add(zToARdoBtn);
 		frmDictionary.getContentPane().add(zToARdoBtn);
-		
-		JList wordList = new JList();
+
+		jsonArray = GSONread.returnWords();
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		JList wordList = new JList(jsonArray.toArray());
 		wordList.setBounds(6, 119, 233, 388);
 		wordList.setFont(new Font("Chalkboard", Font.PLAIN, 12));
-		frmDictionary.getContentPane().add(wordList);
-		
-		JLabel lblNewLabel = new JLabel("Dictionary");
-		lblNewLabel.setBounds(260, 11, 332, 61);
-		lblNewLabel.setFont(new Font("Chalkboard", Font.BOLD, 47));
-		frmDictionary.getContentPane().add(lblNewLabel);
-		
-		JLabel lblWord = new JLabel("Word");
-		lblWord.setFont(new Font("Dialog", Font.BOLD, 28));
-		lblWord.setBounds(270, 75, 332, 45);
-		frmDictionary.getContentPane().add(lblWord);
-		
-		JLabel lblDefinitions = new JLabel("Definitions");
-		lblDefinitions.setFont(new Font("Dialog", Font.BOLD, 28));
-		lblDefinitions.setBounds(270, 152, 332, 45);
-		frmDictionary.getContentPane().add(lblDefinitions);
-		
-		JLabel lblSynonyms = new JLabel("Synonyms");
-		lblSynonyms.setFont(new Font("Dialog", Font.BOLD, 28));
-		lblSynonyms.setBounds(270, 318, 332, 45);
-		frmDictionary.getContentPane().add(lblSynonyms);
-		
-		JLabel lblAntonyms = new JLabel("Antonyms");
-		lblAntonyms.setFont(new Font("Dialog", Font.BOLD, 28));
-		lblAntonyms.setBounds(270, 399, 332, 45);
-		frmDictionary.getContentPane().add(lblAntonyms);
-		
+
+		JScrollPane listScroller = new JScrollPane();
+		listScroller.setViewportView(wordList);
+		listScroller.setBounds(6, 119, 233, 388);
+		wordList.setLayoutOrientation(JList.VERTICAL);
+		frmDictionary.getContentPane().add(listScroller);
+
 		wordInfoDisplay = new JTextField();
 		wordInfoDisplay.setBounds(251, 70, 429, 437);
 		wordInfoDisplay.setFont(new Font("Chalkboard", Font.PLAIN, 13));
@@ -160,6 +158,11 @@ public class GUIFrame {
 		wordInfoDisplay.setEditable(false);
 		frmDictionary.getContentPane().add(wordInfoDisplay);
 		wordInfoDisplay.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Dictionary");
+		lblNewLabel.setBounds(260, 11, 332, 61);
+		lblNewLabel.setFont(new Font("Chalkboard", Font.BOLD, 47));
+		frmDictionary.getContentPane().add(lblNewLabel);
 
 	}
 }
