a76410a506f110075ed0d0ee545dc024cf161d09
David Rogers <drogers@ucvts.tec.nj.us>
2020-03-10 08:23:25 -0400

    definition and part of speech fixed

--- src/Definition.java
+++ src/Definition.java
@@ -1,22 +1,17 @@
-public class Definition {
+public class Definition{
 	private String definition;
 	private String partOfSpeech;
-
+	
 	public Definition(String definition, String partOfSpeech) {
 		this.definition = definition;
 		this.partOfSpeech = partOfSpeech;
 	}
-
+	
 	public String getDefinition() {
 		return definition;
 	}
-
+	
 	public String getPartOfSpeech() {
 		return partOfSpeech;
-	}
-
-	@Override
-	public String toString() {
-		return "Definitions: " + definition + "/nPart of Speech: " + partOfSpeech;
 	}
 }
